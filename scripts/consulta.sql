cbc50ca9c78f838de0481072da930f90cd7a87b1
leo-oliveira2 <heinen126@gmail.com>
2024-09-21 15:35:00 -0300

    Alteração nome -> razao_social

--- scripts/consulta.sql
+++ scripts/consulta.sql
@@ -1,4 +1,4 @@
-SELECT c.id_cliente, c.razao_social as nome, t.numero_telefone as
+SELECT c.id_cliente, c.razao_social, t.numero_telefone as
 FROM clientes c
 JOIN telefones t ON c.id_cliente = t.id_cliente
 JOIN estados e ON c.estado_id = e.id_estado
